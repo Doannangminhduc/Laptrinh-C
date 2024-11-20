f910235cdd19a75a38bdb9215b1e25e9909bcc51
Doannangminhduc <givaytroi2691@gmail.com>
2024-11-21 03:35:48 +0700

    Bài tập 3 SS2 laptrinhC

---
+++ Baitap_3.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int a = 25;
+    int b = 5;
+
+   
+    int sum = a + b;
+   
+    int difference = a - b;
+   
+    int product = a * b;
+   
+    int quotient = a / b;
+
+    printf("Tong cua %d va %d la: %d\n", a, b, sum);
+  
+    printf("Hieu cua %d va %d la: %d\n", a, b, difference);
+    
+	printf("Tich cua %d va %d la: %d\n", a, b, product);
+   
+    printf("Thuong cua %d va %d la: %d\n", a, b, quotient);
+
+    return 0;
+}
+
