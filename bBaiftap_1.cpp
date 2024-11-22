38f5c4c018ba219ac13fae74443b8f2f23099f88
Doannangminhduc <givaytroi2691@gmail.com>
2024-11-22 11:00:51 +0700

    Bài tập 1 SS3 lap trinhC

---
+++ bBaiftap_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string ten;
+
+    cout << "Nhap ten: ";
+    cin >> ten;
+
+    cout << "Xin chao " << ten << endl;
+
+    return 0;
+}
+
